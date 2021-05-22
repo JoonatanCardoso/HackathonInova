87dcddd4bf0581d2aaa36535865536d81548e02a
Lucas Gabriel Lima <eslucasgabriel@gmail.com>
2021-05-22 12:35:45 -0300

    header ADM

--- src/pages/admin/HomeAdm.vue
+++ src/pages/admin/HomeAdm.vue
@@ -1,11 +1,11 @@
 <template>
   <q-page>
-    <div class="row justify-center q-mt-md">
-      <div class="col-lg-8 q-my-lg">
-        Olá, bem vindo nome do fulano
+    <div class="row justify-center ">
+      <div class="col-lg-8 q-my-lg text-subtitle1">
+        Olá, bem vindo(a) <span class="text-weight-bold text-primary">{{nomeUser}}</span>
       </div>
-            <div class="col-lg-2 q-my-lg">
-        Olá, bem vindo nome do fulano
+            <div class="col-lg-2 q-my-lg text-subtitle1">
+       {{diaHoje}} - {{dataHoje}}
       </div>
     </div>
   </q-page>
@@ -13,6 +13,40 @@
 
 <script>
 export default {
-  name: 'HomeAdm'
+  name: 'HomeAdm',
+  data () {
+    return {
+      nomeUser: 'Administrador',
+      dataHoje: '',
+      diaHoje: ''
+    }
+  },
+  mounted () {
+    this.pegarData()
+    this.pegaDia()
+  },
+  methods: {
+    pegarData () {
+      this.dataHoje = new Date().toLocaleDateString()
+      this.diaHoje = new Date().getDay()
+    },
+    pegaDia () {
+      if (this.diaHoje === 0) {
+        this.diaHoje = 'Domingo'
+      } else if (this.diaHoje === 1) {
+        this.diaHoje = 'Segunda-Feira'
+      } else if (this.diaHoje === 2) {
+        this.diaHoje = 'Terça-Feira'
+      } else if (this.diaHoje === 3) {
+        this.diaHoje = 'Quarta-Feira'
+      } else if (this.diaHoje === 4) {
+        this.diaHoje = 'Quinta-Feira'
+      } else if (this.diaHoje === 5) {
+        this.diaHoje = 'Sexta-Feira'
+      } else if (this.diaHoje === 6) {
+        this.diaHoje = 'Sábado'
+      }
+    }
+  }
 }
 </script>
