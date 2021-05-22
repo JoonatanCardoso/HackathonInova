bf3b5bedbcacc11797cc1a7541aa9d4080ece9ec
Josseandro <farias.josseandro@gmail.com>
2021-05-22 12:53:22 -0400

    ✨ NEW FEATURE: add base usuarios store

---
+++ src/store/usuarios/mutations.js
@@ -0,0 +1,12 @@
+export const SET_USUARIOS = (state, val) => {
+  state.usuarios = val
+}
+export const SET_USUARIO = (state, val) => {
+  state.usuario = val
+}
+export const setEmpresas = (state, val) => {
+  state.empresas = val
+}
+export const setEmpresa = (state, val) => {
+  state.empresa = val
+}
