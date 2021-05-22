958dec478582a8fe665ff31e42626e9ef300f918
Lucas Gabriel Lima <eslucasgabriel@gmail.com>
2021-05-22 14:09:40 -0300

    cor e concordo com os termos

--- src/pages/admin/HomeAdm.vue
+++ src/pages/admin/HomeAdm.vue
@@ -1,40 +1,40 @@
 <template>
   <q-page>
-    <div class="row justify-center ">
-      <div class="col-lg-8 q-my-lg text-subtitle1">
+    <div class="row justify-center q-px-sm ">
+      <div class="col-lg-8 col-md-8 q-my-lg text-subtitle1">
         Olá, bem vindo(a)
         <span class="text-weight-bold text-primary">{{ nomeUser }}</span>
       </div>
-      <div class="col-lg-2 q-my-lg text-subtitle1">
+      <div class="col-lg-2 col-md-2 q-my-lg text-subtitle1">
         {{ diaHoje }} - {{ dataHoje }}
       </div>
     </div>
     <div class="row justify-center">
-      <div class="col-lg-2 q-px-sm">
+      <div class="col-lg-2 col-md-3 q-px-sm">
         <q-card>
           <q-card bordered class="shadow-0 bg-white border">
             <q-card-section class="text-h4">
               555
               <br />
-              <span class="text-subtitle1 text-grey-7">Empresas pequenas</span>
+              <span class="text-subtitle1 text-grey-7">Emp. Pequenas</span>
             </q-card-section>
           </q-card>
         </q-card>
       </div>
-      <div class="col-lg-2 q-px-sm">
+      <div class="col-lg-2 col-md-3 q-px-sm">
         <q-card>
           <q-card bordered class="shadow-0 bg-white border">
             <q-card-section class="text-h4">
               555
               <br />
               <span class="text-subtitle1 text-grey-7"
-                >Empresas Médias/Grandes</span
+                >Emp. Médias/Grandes</span
               >
             </q-card-section>
           </q-card>
         </q-card>
       </div>
-      <div class="col-lg-2 q-px-sm">
+      <div class="col-lg-2 col-md-2 q-px-sm">
         <q-card>
           <q-card bordered class="shadow-0 bg-white border">
             <q-card-section class="text-h4">
@@ -45,7 +45,7 @@
           </q-card>
         </q-card>
       </div>
-      <div class="col-lg-2 q-px-sm">
+      <div class="col-lg-2 col-md-2 q-px-sm">
         <q-card>
           <q-card bordered class="shadow-0 bg-white border">
             <q-card-section class="text-h4">
@@ -58,7 +58,7 @@
       </div>
     </div>
     <div class="row justify-center q-mt-md">
-      <div class="col-lg-8 q-px-sm">
+      <div class="col-lg-8 col-md-10 q-px-sm">
         <q-card class="">
           <div class="row justify-center q-px-sm">
             <div class="col-lg-12  q-mt-md text-h5 text-grey-7">
@@ -70,34 +70,34 @@
             </div>
           </div>
           <div class="row justify-center q-px-sm q-gutter-x-sm">
-            <div class="col-lg-3 q-mb-sm">
+            <div class="col-lg-3 col-md-3 q-mb-sm">
               <div class="text-justify q-px-xs">
                 <q-badge color="red">Qtd - 4</q-badge> - Cultivo de outras
                 plantas de lavoura permanente não especificadas anteriormente
               </div>
             </div>
-            <div class="col-lg-3 q-mb-sm">
+            <div class="col-lg-3 col-md-3 q-mb-sm">
               <div class="text-justify q-px-xs">
                 <q-badge color="red">Qtd - 4</q-badge> - Produção de produtos
                 não-madeireiros não especificados anteriormente em florestas
                 plantadas
               </div>
             </div>
-            <div class="col-lg-3 q-mb-sm">
+            <div class="col-lg-3 col-md-3 q-mb-sm">
               <div class="text-justify q-px-xs">
                 <q-badge color="red">Qtd - 4</q-badge> - Extração de minérios de
                 cobre, chumbo, zinco e outros minerais metálicos não-ferrosos
                 especificados anteriormente.
               </div>
             </div>
-            <div class="col-lg-3 q-mb-sm">
+            <div class="col-lg-3 col-md-3 q-mb-sm">
               <div class="text-justify q-px-xs">
                 <q-badge color="red">Qtd - 4</q-badge> - Confecção, sob medida,
                 de peças do vestuário, exceto roupas íntimas SIM/NÃO O material
                 será fornecido pelo executante do serviço?
               </div>
             </div>
-            <div class="col-lg-3 q-mb-sm">
+            <div class="col-lg-3 col-md-3 q-mb-sm">
               <div class="text-justify q-px-xs">
                 <q-badge color="red">Qtd - 4</q-badge> - Fabricação de produtos
                 de papel, cartolina, papel-cartão e papelão ondulado para uso
@@ -105,7 +105,7 @@
                 contínuo
               </div>
             </div>
-            <div class="col-lg-3 q-mb-sm">
+            <div class="col-lg-3 col-md-3 q-mb-sm">
               <div class="text-justify q-px-xs">
                 <q-badge color="red">Qtd - 4</q-badge> - Coquerias
               </div>
@@ -114,8 +114,8 @@
         </q-card>
       </div>
     </div>
-    <div class="row justify-center q-mt-md">
-      <div class="col-lg-8 q-px-sm">
+    <div class="row justify-center q-mt-lg q-mb-lg">
+      <div class="col-lg-8 col-md-10 q-px-sm">
         <q-card class="">
           <div class="row justify-center q-px-sm">
             <div class="col-lg-12  q-mt-md text-h5 text-grey-7">
@@ -127,32 +127,32 @@
             </div>
           </div>
           <div class="row justify-center q-px-sm q-gutter-x-sm">
-            <div class="col-lg-3 q-mb-sm">
+            <div class="col-lg-3 col-md-3 q-mb-sm">
               <div class="text-justify q-px-xs">
                 <q-badge color="red">Qtd - 4</q-badge> - Eventos
               </div>
             </div>
-            <div class="col-lg-3 q-mb-sm">
+            <div class="col-lg-3 col-md-3 q-mb-sm">
               <div class="text-justify q-px-xs">
                 <q-badge color="red">Qtd - 4</q-badge> - Cursos
               </div>
             </div>
-            <div class="col-lg-3 q-mb-sm">
+            <div class="col-lg-3 col-md-3 q-mb-sm">
               <div class="text-justify q-px-xs">
                 <q-badge color="red">Qtd - 4</q-badge> - Oficinas
               </div>
             </div>
-            <div class="col-lg-3 q-mb-sm">
+            <div class="col-lg-3 col-md-3 q-mb-sm">
               <div class="text-justify q-px-xs">
                 <q-badge color="red">Qtd - 4</q-badge> - Palestras
               </div>
             </div>
-            <div class="col-lg-3 q-mb-sm">
+            <div class="col-lg-3 col-md-3 q-mb-sm">
               <div class="text-justify q-px-xs">
                 <q-badge color="red">Qtd - 4</q-badge> - Consultorias
               </div>
             </div>
-            <div class="col-lg-3 q-mb-sm">
+            <div class="col-lg-3 col-md-3 q-mb-sm">
               <div class="text-justify q-px-xs">
                 <q-badge color="red">Qtd - 4</q-badge> - Outros
               </div>
