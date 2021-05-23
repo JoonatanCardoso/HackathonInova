658339c997f1b5bc0e18acfd8cc62600ddce1b8f
Jonatan <jonatancardosoc@gmail.com>
2021-05-23 01:09:45 -0300

    alinhamento de ideias

--- src/pages/empresa/Orcamentos.vue
+++ src/pages/empresa/Orcamentos.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page padding>
     <div class="row justify-center">
-        <div class="col-xl-10 col-lg-6 col-md-8 col-sm-6 col-xs-10 text-h4 q-my-lg">
+        <div class="col-xl-10 col-lg-6 col-md-8 col-sm-6 col-xs-10 text-bold text-h4 q-my-lg">
           Orçamentos
         </div>
     </div>
@@ -9,7 +9,7 @@
       <q-btn class="col-xl-3 col-lg-3 col-md-3 col-sm-11 col-xs-11 bg-primary text-bold text-white shadow-2 text-center q-pa-sm"
       @click="lista = true; meus = false; aprovacao = false"
       >
-        <span>Lista de Orçamentos</span>
+        <span>Propostas de Orçamentos</span>
       </q-btn>
       <q-btn class="col-xl-3 col-lg-3 col-md-3 col-sm-11 col-xs-11 bg-primary text-bold text-white shadow-2 text-center q-pa-sm"
       @click="lista = false; meus = true; aprovacao = false"
@@ -19,7 +19,7 @@
       <q-btn class="col-xl-3 col-lg-3 col-md-3 col-sm-11 col-xs-11 bg-primary text-bold text-white shadow-2 text-center q-pa-sm"
       @click="lista = false; meus = false; aprovacao = true"
       >
-        <span>Aprovação de Orçamentos</span>
+        <span>Histórico de orçamento</span>
       </q-btn>
 
       <div v-if="lista == true" class="col-xl-9 col-lg-9 col-md-9 col-sm-11 col-xs-11 q-mt-lg" style="border:solid 2px; border-radius: 5px; color: #ffe600">
